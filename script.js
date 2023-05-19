0eb576e657418fd3a375bbdd89191a6dea341738
madjonga <madjonga123@gmail.com>
2023-05-19 12:57:39 +0300

    Add add-buttons functionality

--- script.js
+++ script.js
@@ -27,6 +27,28 @@ function addManga() {
 }
 
 const addAnimeBtn = document.getElementById('add-anime')
+const addMangaBtn = document.getElementById('add-manga')
+const addBookBtn = document.getElementById('add-book')
+const form = document.querySelector('form')
+
+
 addAnimeBtn.addEventListener('click', function(e) {
-    
+    form.style.display = 'grid'
+    document.getElementById('title').placeholder = 'Anime name'
+    document.getElementById('number').placeholder = 'Number of episodes'
+    document.getElementById('pages-episodes').textContent = 'Watched?'
+})
+
+addMangaBtn.addEventListener('click', function(e) {
+    form.style.display = 'grid'
+    document.getElementById('title').placeholder = 'Manga name'
+    document.getElementById('number').placeholder = 'Number of pages'
+    document.getElementById('pages-episodes').textContent = 'Read?'
+})
+
+addBookBtn.addEventListener('click', function(e) {
+    form.style.display = 'grid'
+    document.getElementById('title').placeholder = 'Book name'
+    document.getElementById('number').placeholder = 'Number of pages'
+    document.getElementById('pages-episodes').textContent = 'Read?'
 })
