9120aba331a2150d7379f2c73218a035d8738e29
madjonga <madjonga123@gmail.com>
2023-05-19 19:39:50 +0300

    Attempt to add add-button functionality

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
 let myBookLibrary = [];
 let myAnimeLibrary = [];
 let myMangaLibrary = [];
-
-function Book() {
-  // the constructor...
-}
-
-function Anime() {
-    // the constructor...
-}
-
-function Manga() {
-    // the constructor...
-}
-
-function addBook() {
-  // do stuff here
-}
-  
-function addAnime() {
-    // do stuff here
-}
-  
-function addManga() {
-    // do stuff here
-}
+let contentType = ''
 
 const addAnimeBtn = document.getElementById('add-anime')
 const addMangaBtn = document.getElementById('add-manga')
 const addBookBtn = document.getElementById('add-book')
+const addContentToLibrary = document.getElementById('submit')
 const form = document.querySelector('form')
 
-
 addAnimeBtn.addEventListener('click', function(e) {
+    contentType = 'anime'
     form.style.display = 'grid'
     document.getElementById('title').placeholder = 'Anime name'
     document.getElementById('number').placeholder = 'Number of episodes'
@@ -40,6 +18,7 @@ addAnimeBtn.addEventListener('click', function(e) {
 })
 
 addMangaBtn.addEventListener('click', function(e) {
+    contentType = 'manga'
     form.style.display = 'grid'
     document.getElementById('title').placeholder = 'Manga name'
     document.getElementById('number').placeholder = 'Number of pages'
@@ -47,8 +26,68 @@ addMangaBtn.addEventListener('click', function(e) {
 })
 
 addBookBtn.addEventListener('click', function(e) {
+    contentType = 'book'
     form.style.display = 'grid'
     document.getElementById('title').placeholder = 'Book name'
     document.getElementById('number').placeholder = 'Number of pages'
     document.getElementById('pages-episodes').textContent = 'Read?'
 })
+
+addContentToLibrary.addEventListener('click', function(e) {
+    if (document.getElementById('title').value === '') {
+        return
+    }
+    let newElement = document.createElement('tr')
+    newElement.id = 'newElement'
+    let thName = document.createElement('th')
+    let tdAuthor = document.createElement('td')
+    let tdNumber = document.createElement('td')
+    let tdSeen = document.createElement('td')
+    let tdDelete = document.createElement('td')
+
+    newElement.appendChild(thName)
+    newElement.appendChild(tdAuthor)
+    newElement.appendChild(tdNumber)
+    newElement.appendChild(tdSeen)
+    newElement.appendChild(tdDelete)
+
+    thName.textContent = document.getElementById('title').value
+    tdAuthor.textContent = document.getElementById('author').value
+    tdNumber.textContent = document.getElementById('number').value
+    let check = document.createElement('input')
+    check.type = 'checkbox'
+    check.checked = document.getElementById('checkmark').checked
+    tdSeen.appendChild(check)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.id = 'delete-btn'
+    deleteBtn.textContent = 'Delete'
+    tdDelete.appendChild(deleteBtn)
+
+    if (contentType == 'anime') {
+        document.getElementById('anime-content').appendChild(newElement)
+    }
+})
+
+function Book() {
+    // the constructor...
+}
+  
+  function Anime() {
+      // the constructor...
+}
+  
+  function Manga() {
+      // the constructor...
+  }
+  
+  function addBook() {
+    // do stuff here
+  }
+    
+  function addAnime() {
+      // do stuff here
+  }
+    
+  function addManga() {
+      // do stuff here
+  }
